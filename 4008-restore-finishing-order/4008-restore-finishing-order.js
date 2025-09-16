11c173d978ee57d470f299b25ecbea4ad2556591
Adhil p <140001507+adhilX@users.noreply.github.com>
2025-09-16 14:45:28 +0530

    Time: 1 ms (86.6%), Space: 58.2 MB (44.09%) - LeetHub

---
+++ 4008-restore-finishing-order/4008-restore-finishing-order.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} order
+ * @param {number[]} friends
+ * @return {number[]}
+ */
+var recoverOrder = function(order, friends) {
+    let result =[]
+    for(let i =0 ; i < order.length ;i++){
+      for(let j = 0 ; j< friends.length ;j++){
+        if(order[i]== friends[j]){
+            result.push(friends[j])
+        }
+      }
+    }
+    return result
+};
