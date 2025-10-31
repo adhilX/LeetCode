db95a61a73853225c3fde53bdc2d02788a786e5d
Adhil p <140001507+adhilX@users.noreply.github.com>
2025-10-31 12:13:52 +0530

    Time: 1 ms (81.63%), Space: 57.8 MB (25.41%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -8,7 +8,8 @@ var jump = function(nums) {
     let farthest = 0;
 
 for(let i = 0 ; i <nums.length-1;i++){
-    farthest = Math.max(farthest,i+nums[i])
+  
+  farthest = Math.max(farthest,i+nums[i])
 
  if (i === end) {
             jumps++;
