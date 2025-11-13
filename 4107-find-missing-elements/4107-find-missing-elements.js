a2c6a817636b5a96fa8159d03bc9eeca327c3568
Adhil p <140001507+adhilX@users.noreply.github.com>
2025-11-13 15:32:58 +0530

    Time: 1 ms (98.81%), Space: 58.6 MB (24.7%) - LeetHub

---
+++ 4107-find-missing-elements/4107-find-missing-elements.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function(nums) {
+    
+  const max = Math.max(...nums)
+  const min = Math.min(...nums)
+   const set = new Set(nums);
+
+let arr = []
+
+for(let i = min ;i<= max ; i++){
+  if (!set.has(i)) {
+      arr.push(i);
+    }
+}
+
+return arr
+};
