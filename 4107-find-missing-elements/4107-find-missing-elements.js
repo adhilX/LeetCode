2776d9138f0eeefc1902b814acdf966f76e93b40
Adhil p <140001507+adhilX@users.noreply.github.com>
2025-11-13 15:36:36 +0530

    Time: 3 ms (65.25%), Space: 58.3 MB (54.34%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.js
+++ 4107-find-missing-elements/4107-find-missing-elements.js
@@ -6,12 +6,11 @@ var findMissingElements = function(nums) {
     
   const max = Math.max(...nums)
   const min = Math.min(...nums)
-   const set = new Set(nums);
 
 let arr = []
 
 for(let i = min ;i<= max ; i++){
-  if (!set.has(i)) {
+  if (!nums.includes(i)) {
       arr.push(i);
     }
 }
