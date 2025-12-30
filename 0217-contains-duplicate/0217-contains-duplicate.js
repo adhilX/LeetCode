1fd4024da5d746bcf097e34acdcc83e5cc3709e5
Adhil p <140001507+adhilX@users.noreply.github.com>
2025-12-30 18:32:24 +0530

    Time: 112 ms (5.16%), Space: 82.1 MB (5.03%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = {}
+
+    for(let i = 0 ; i < nums.length ; i++){
+        map[nums[i]] = (map[nums[i]] || 0)+1
+    }
+
+
+    for(let key in map){
+        if(map[key]> 1)return true
+    }
+
+    return false
+};
